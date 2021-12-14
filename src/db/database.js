aefca63256dc22a2b41e2132523cea65859d23ac
AranBeitia <arantza.beitia@gmail.com>
2021-12-14 17:08:51 +0100

    db connection

---
+++ src/db/database.js
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose'
+// require('dotenv').config({ path: 'variables.env' })
+;(async () => {
+	try {
+		const db = await mongoose.connect('mongodb://127.0.0.1:27017/crud-mongo', {
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+		})
+		console.log('DB connected to', db.connections)
+	} catch (error) {
+		console.log(error)
+	}
+})()
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import app from './app'
+import './db/database'
 
 app.listen(3000)
 console.log('Server on port', 3000)
