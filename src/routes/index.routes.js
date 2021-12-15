883a5750f7d4b79a77f243a85f1d86bb43c7bafe
AranBeitia <arantza.beitia@gmail.com>
2021-12-15 21:06:27 +0100

    crud controllers

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,55 +1,25 @@
 import { Router } from 'express'
-import Task from '../models/Task'
-
 const router = Router()
 
-router.get('/', async (req, res) => {
-	const tasks = await Task.find().lean()
+import {
+	renderTasks,
+	addTasks,
+	renderTaskEdit,
+	editTask,
+	deleteTask,
+	toggleTask,
+} from '../controllers/tasks.controller'
 
-	res.render('index', { tasks })
-})
+router.get('/', renderTasks)
 
-router.post('/tasks/add', async (req, res) => {
-	try {
-		const task = Task(req.body)
-		await task.save()
-		res.redirect('/')
-	} catch (error) {
-		console.log(error)
-	}
-})
+router.post('/tasks/add', addTasks)
 
-router.get('/about', (req, res) => {
-	res.render('about')
-})
+router.get('/tasks/:id/edit', renderTaskEdit)
 
-router.get('/edit/:id', async (req, res) => {
-	try {
-		const task = await Task.findById(req.params.id).lean()
-		res.render('edit', { task })
-	} catch (error) {
-		console.log(error.message)
-	}
-})
+router.post('/tasks/:id/edit', editTask)
 
-router.post('/edit/:id', async (req, res) => {
-	const { id } = req.params
-	await Task.findByIdAndUpdate(id, req.body)
-	res.redirect('/')
-})
+router.get('/tasks/:id/delete', deleteTask)
 
-router.get('/delete/:id', async (req, res) => {
-	const { id } = req.params
-	await Task.findByIdAndDelete(id)
-	res.redirect('/')
-})
-
-router.get('/toggleDone/:id', async (req, res) => {
-	const { id } = req.params
-	const task = await Task.findById(id)
-	task.done = !task.done
-	await task.save()
-	res.redirect('/')
-})
+router.get('/tasks/:id/toggleDone', toggleTask)
 
 export default router
